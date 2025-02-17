6aa8a03e7fdddc27c7ba018f00dbc43b583b1d98
Gabriel <stewarti3@yahoo.com>
2025-02-17 23:44:09 +0100

    update: nav ui

--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Home, BookOpenText, Settings, Github } from 'lucide-react'
 
 const NavBar = () => {
   return (
-    <div className='h-[5rem] w-screen border-b px-10 py-4 border-neutral-900 bg-black bg-opacity-20 backdrop-blur-lg fixed font-display'>
+    <div className='h-[4rem] w-screen border-b px-10 py-2 border-neutral-950 bg-black bg-opacity-20 backdrop-blur-sm fixed font-display'>
       <div className="flex flex-row items-center gap-4">
         <div className="inline-flex gap-2 text-white text-xl items-center">
           <a href="#" className='inline-flex gap-2 items-center group'>
@@ -21,8 +21,8 @@ const NavBar = () => {
           <a href="#" className="inline-flex gap-2 items-center hover:opacity-60 transition-all duration-200">
             <Settings size={20} /> Settings
           </a>
-          <a href="#" className="inline-flex gap-2 items-center hover:opacity-60 transition-all duration-200">
-            <button className="flex flex-row bg-transparent border border-neutral-900 rounded-full px-6 py-2 text-center justify-center items-center gap-1 hover:bg-neutral-800 transition-all duration-200">
+          <a href="#" className="inline-flex gap-2 items-center transition-all duration-200">
+            <button className="flex flex-row bg-transparent border border-neutral-700 rounded-xl px-6 py-2 text-center justify-center items-center gap-1 bg-neutral-900 hover:bg-neutral-800 transition-all duration-200">
               <Github size={18} /> GitHub
             </button>
           </a>
