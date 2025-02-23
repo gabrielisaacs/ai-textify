fec64823d5f004abbb0ac69a62be078acc5daf8b
Gabriel <stewarti3@yahoo.com>
2025-02-24 00:45:45 +0100

    reduced text width

--- app/src/components/WelcomeScreen.jsx
+++ app/src/components/WelcomeScreen.jsx
@@ -6,7 +6,7 @@ const WelcomeScreen = () => {
     <div className="flex flex-col items-center justify-center h-full text-center">
       <img src="/logo.svg" alt="Textify Logo" className="w-32 h-32 lg:w-40 lg:h-40 mb-4" />
       <h1 className="text-[3rem] text-white mb-2 font-display">Hi, I'm Textify</h1>
-      <p className="text-center text-lg gap-2 justify-center mt-2 max-w-[25rem] text-neutral-400">
+      <p className="text-center text-lg gap-2 justify-center mt-2 max-w-[20rem] lg:max-w-[25rem] text-neutral-400">
         I can detect language, translate and summarize text. Type below to begin...
       </p>
       <div className="h-[5rem] sm:hidden md:flex"></div>
